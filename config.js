dc53baeaa3df4751cfe9ee422b6a3531d80d5fd3
sandaruwan12345 <146765452+sandaruwan12345@users.noreply.github.com>
2023-10-07 11:03:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -12,22 +12,22 @@ const fs = require('fs-extra')
  global.gurl = 'https://instagram.com/' // add your username 
  global.sudo = process.env.SUDO || '919628516236' 
  global.devs = '919628516236'; 
- global.website = 'https://github.com/SamPandey001/Secktor-Md' //wa.me/+91000000000000 
+ global.website = 'https://github.com/SamPandey001/Secktor-Md' //wa.me/+94779122081
  global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://raw.githubusercontent.com/SecktorBot/Brandimages/main/logos/SocialLogo%201.png' 
  module.exports = { 
-   botname:   process.env.BOT_NAME === undefined ? '𝐒𝐞𝐜𝐤𝐭𝐨𝐫 𝐁𝐨𝐭𝐭𝐨' : process.env.BOT_NAME, 
-   ownername: process.env.OWNER_NAME === undefined ? 'SamPandey001' : process.env.OWNER_NAME,  
-   author:  process.env.PACK_INFO.split(";")[0] === undefined ? 'SamPandey001' : process.env.PACK_INFO.split(";")[0], 
-   auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS, 
-   packname:  process.env.PACK_INFO.split(";")[1] === undefined ? 'Secktor-Md' : process.env.PACK_INFO.split(";")[1],
+   botname:   process.env.BOT_NAME === undefined ? '𝐍𝐑 𝐑𝐀𝐒𝐇𝐔 𝐌𝐎𝐃𝐙' : process.env.BOT_NAME, 
+   ownername: process.env.OWNER_NAME === undefined ? 'RashuManika0715' : process.env.OWNER_NAME,  
+   author:  process.env.PACK_INFO.split(";")[0] === undefined ? 'NR RASHU MODZ....🥺💗' : process.env.PACK_INFO.split(";")[0], 
+   auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? true : process.env.AUTO_READ_STATUS, 
+   packname:  process.env.PACK_INFO.split(";")[1] === undefined ? 'RASHU MODZ' : process.env.PACK_INFO.split(";")[1],
    sessionName:  process.env.SESSION_ID === undefined ? false : process.env.SESSION_ID, 
-   autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION , 
+   autoreaction:  process.env.AUTO_REACTION  === undefined ? true : process.env.AUTO_REACTION , 
    antibadword :  process.env.ANTI_BAD_WORD === undefined ? 'nbwoed' : process.env.ANTI_BAD_WORD, 
    alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? false : process.env.ALWAYS_ONLINE,
-   voice  :  process.env.AUTO_VOICE === undefined ? false : process.env.AUTO_VOICE, 
+   voice  :  process.env.AUTO_VOICE === undefined ? true : process.env.AUTO_VOICE, 
    antifake : process.env.FAKE_COUNTRY_CODE === undefined ? '971' : process.env.FAKE_COUNTRY_CODE, 
    readmessage:  process.env.READ_MESSAGE === undefined ? false : process.env.READ_MESSAGE, 
-   auto_status_saver: process.env.AUTO_STATUS_SAVER === undefined ? false : process.env.AUTO_STATUS_SAVER, 
+   auto_status_saver: process.env.AUTO_STATUS_SAVER === undefined ? true : process.env.AUTO_STATUS_SAVER, 
    HANDLERS:  process.env.PREFIX === undefined ? ['.'] : process.env.PREFIX, 
    warncount : process.env.WARN_COUNT === undefined ? 3 : process.env.WARN_COUNT, 
    disablepm:  process.env.DISABLE_PM === undefined ? false : process.env.DISABLE_PM, 
